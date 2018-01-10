b976b7a72676cecec6dff7bf290c4dde10087149
Belov Roman <otezzz1982@gmail.com>
2018-01-11 01:35:02 +0200

    Домашка № 7.2

--- src/cookie.js
+++ src/cookie.js
@@ -43,19 +43,70 @@ let tableHtml =
 
 import { createCookie, deleteCookie } from './index';
 
-filterNameInput.addEventListener('keyup', () => {
+// function isMatching(full, chunk) {
+//
+//     if (chunk === '') {
+//         return true;
+//
+//     }
+//
+//     for (var key in full) {
+//         if (key.trim().toLowerCase().indexOf(chunk.toLowerCase()) + 1) {
+//             return true;
+//         }
+//     }
+//
+//     return false;
+// }
 
+function getCookie() {
+    var cookieArr = document.cookie.split('; ');
+    const params = cookieArr.reduce((prev, curr) => {
+        const [name, value] = curr.split('=');
+        prev[name] = value;
+
+        return prev;
+    }, {});
+
+    return params;
+}
+
+filterNameInput.addEventListener('keyup', () => {
+    let filtValue = filterNameInput.value;
+    let trColl = listTable.querySelectorAll('tr');
+    let arrCookie = getCookie();
+
+    for (var i = 0; i < trColl.length; i++) {
+        trColl[i].remove();
+    }
+
+    for (var key in arrCookie) {
+        let keyCookie = key.trim().toLowerCase().indexOf(filtValue.toLowerCase()) + 1;
+        let valueCookie = arrCookie[key].trim().toLowerCase().indexOf(filtValue.toLowerCase()) + 1;
+
+        if (keyCookie || valueCookie) {
+
+            listTable.insertAdjacentHTML('beforeend', tableHtml);
+            listTable.querySelector('tr:last-child .name').innerText = key;
+            listTable.querySelector('tr:last-child .value').innerText = arrCookie[key];
+            listTable.querySelector('tr:last-child').classList.add(key);
+            listTable.querySelector('tr:last-child button').setAttribute('id', key);
+        }
+    }
 });
 
-
 addButton.addEventListener('click', () => {
-    var names = listTable.querySelectorAll('.name');
+    let names = listTable.querySelectorAll('.name');
+    let nameFilter = addNameInput.value.trim().toLowerCase().indexOf(filterNameInput.value.toLowerCase()) + 1;
+    let valueFilter = addValueInput.value.trim().toLowerCase().indexOf(filterNameInput.value.toLowerCase()) + 1;
 
-    listTable.insertAdjacentHTML('beforeend', tableHtml);
-    listTable.querySelector('tr:last-child .name').innerText = addNameInput.value;
-    listTable.querySelector('tr:last-child .value').innerText = addValueInput.value;
-    listTable.querySelector('tr:last-child').classList.add(addNameInput.value);
-    listTable.querySelector('tr:last-child button').setAttribute('id', addNameInput.value);
+    if (nameFilter || valueFilter) {
+        listTable.insertAdjacentHTML('beforeend', tableHtml);
+        listTable.querySelector('tr:last-child .name').innerText = addNameInput.value;
+        listTable.querySelector('tr:last-child .value').innerText = addValueInput.value;
+        listTable.querySelector('tr:last-child').classList.add(addNameInput.value);
+        listTable.querySelector('tr:last-child button').setAttribute('id', addNameInput.value);
+    }
     createCookie(addNameInput.value, addValueInput.value);
 
     for (var i = 0; i < names.length; i++) {
